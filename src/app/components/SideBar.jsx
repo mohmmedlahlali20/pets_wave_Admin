531618d86a33476c0b1daa38077a73ffe191826a
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-29 12:59:27 +0100

    fix(components): update SideBar component

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -30,7 +30,7 @@ export default function SideBar() {
           </li>
           <li>
             <Link
-              href="/products"
+              href="/dashboard/Pets"
               className="flex items-center px-4 py-3 text-sm font-medium rounded-lg hover:bg-gray-800 transition"
             >
               <Cat className="w-5 h-5 mr-3" />

ed9ef519c1b9016936a48a8f8ec5fe2b5cc6f26e
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-02 02:28:55 +0100

    Update path.js

--- src/app/axios/path.js
+++ src/app/axios/path.js
@@ -2,17 +2,23 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const path = axios.create({
-
   baseURL: process.env.NEXT_PUBLIC_URL_API,
 });
 
 path.interceptors.request.use(
   function (config) {
-    config.headers["Content-Type"] = "multipart/form-data"
     const token = Cookies.get("token");
+
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
+
+    if (config.data instanceof FormData) {
+      config.headers["Content-Type"] = "multipart/form-data";
+    } else {
+      config.headers["Content-Type"] = "application/json";
+    }
+
     return config;
   },
   function (err) {

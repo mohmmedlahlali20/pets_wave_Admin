78c704c06d0aa0deddc77a69b17017b0940f4199
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-04 16:53:10 +0100

    Update getAllCategory.js

--- src/app/dashboard/server/getAllCategory.js
+++ src/app/dashboard/server/getAllCategory.js
@@ -1,11 +1,8 @@
 import path from "@/app/axios/path";
 
 export async function getAllcategory() {
-    try {
+
         const res = await path.get('category/GetAll')
         return res.data || []
-    } catch (err) {
-        console.log('err get category', err)
-        return []
-    }
+    
 }

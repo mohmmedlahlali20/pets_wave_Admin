ca3282360f62dcc051bf40f7402358ee7a4bc82b
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-05 16:36:40 +0100

    Update GetAllPets.js

--- src/app/dashboard/server/GetAllPets.js
+++ src/app/dashboard/server/GetAllPets.js
@@ -1,11 +1,9 @@
 import path from "@/app/axios/path";
 
 export async function getAllPets(){
-    try {
+ 
         const res = await path.get("pets/findAllForAdmin");
         return res.data.pets || [];
-      } catch (error) {
-        console.error("Erreur lors de la récupération des produits:", error);
-        return [];
-      }
+   
+   
 }

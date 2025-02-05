a3de517eca64b6d7bb4f933c33479fda9c9da2a9
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-05 14:59:30 +0100

    Update authSlice.js

--- src/app/redux/slice/authSlice.js
+++ src/app/redux/slice/authSlice.js
@@ -1,24 +1,26 @@
 import path from "@/app/axios/path";
+import { GetAllUsers } from "@/app/dashboard/server/getAllUsers";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
 import Cookies from "js-cookie";
+
+export const getUsers = createAsyncThunk(
+    "auth/getUsers",
+    async (_, { rejectWithValue }) => {
+        try {
+            const users = await GetAllUsers();
+            return users || [];
+        } catch (err) {
+            console.log(err);
+            return rejectWithValue("Erreur lors de la récupération des utilisateurs");
+        }
+    }
+);
 
 export const loginUser = createAsyncThunk(
     "auth/loginUser",
-    async (
-        {
-            email,
-            password
-        },
-        {
-            rejectWithValue
-        }
-    ) => {
+    async ({ email, password }, { rejectWithValue }) => {
         try {
             const response = await path.post("auth/login", { email, password });
-            console.log('====================================');
-            console.log(response.data);
-            console.log('====================================');
 
             if (response.data) {
                 Cookies.set("token", response.data.token, { expires: 7 });
@@ -28,14 +30,16 @@ export const loginUser = createAsyncThunk(
         } catch (error) {
             return rejectWithValue(error.response?.data?.message || "Une erreur s'est produite");
         }
-    });
+    }
+);
 
 const initialState = {
     user: null,
+    users: [],  
     token: Cookies.get("token") || null,
     loading: false,
     error: null,
-}
+};
 
 const authSlice = createSlice({
     name: "auth",
@@ -44,6 +48,7 @@ const authSlice = createSlice({
         logout: (state) => {
             state.user = null;
             state.token = null;
+            state.users = [];  
             Cookies.remove("token");
         },
     },
@@ -59,6 +64,17 @@ const authSlice = createSlice({
                 state.token = action.payload.token;
             })
             .addCase(loginUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+            .addCase(getUsers.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(getUsers.fulfilled, (state, action) => {
+                state.loading = false;
+                state.users = action.payload; 
+            })
+            .addCase(getUsers.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload;
             });
